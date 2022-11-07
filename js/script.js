3638b285cc593d597305703af3c50480a204bbfc
sabaslote <112454708+sabaslote@users.noreply.github.com>
2022-11-07 12:06:24 -0600

    declaring and adding functions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
+  function myAlertFunction(){
+    alert('Saba Slote:' + input);
+  }
+  var input = document.getElementById('entryinput').value;
+  var output = document.getElementById('textoutput');
+  output.innerHTML = input;
+  var alertButton = document.getElementById('entrybutton');
+  alertButton.addEventListener('click', myAlertFunction);
+}
 
 window.addEventListener('load', init);
