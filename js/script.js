a6420839fd494c4fc653c58fcf80e89a0ecd5c50
sabaslote <112454708+sabaslote@users.noreply.github.com>
2022-11-07 12:27:04 -0600

    creating less confusing varibles

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
   function myAlertFunction(){
-    var input = document.getElementById('entryinput').value;
+    var inputText = document.getElementById('entryinput').value;
 	var output = document.getElementById('textoutput');
-    output.innerHTML = input;
-	alert("Saba Slote: "+ input);
+    output.innerHTML = inputText;
+	alert("Saba Slote: "+ inputText);
   }
-  var alertButton = document.getElementById('entrybutton');
-  alertButton.addEventListener('click', myAlertFunction);
+  var alertDiv = document.getElementById('entrybutton');
+  alertDiv.addEventListener('click', myAlertFunction);
 }
 window.addEventListener('load', init);
