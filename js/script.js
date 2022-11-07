737a2a7a310c35b1f70fb27816c53a0278a70747
sabaslote <112454708+sabaslote@users.noreply.github.com>
2022-11-07 12:24:18 -0600

    fixing organization and replacing lines

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
   function myAlertFunction(){
-    alert('Saba Slote:' + input);
+    var input = document.getElementById('entryinput').value;
+	var output = document.getElementById('textoutput');
+    output.innerHTML = input;
+	alert("Saba Slote: "+ input);
   }
-  var input = document.getElementById('entryinput').value;
-  var output = document.getElementById('textoutput');
-  output.innerHTML = input;
   var alertButton = document.getElementById('entrybutton');
   alertButton.addEventListener('click', myAlertFunction);
 }
-
 window.addEventListener('load', init);
